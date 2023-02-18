20fe3d8f20db5741626679e0d34bca6476b02c29
Kibycz <villalbajoakin@gmail.com>
2023-02-17 21:54:07 -0300

    Codigo limpio

--- src/main/java/com/porfolioWeb/Porfolio/PorfolioApplication.java
+++ src/main/java/com/porfolioWeb/Porfolio/PorfolioApplication.java
@@ -1,10 +1,6 @@
 package com.porfolioWeb.Porfolio;
 
-/*import com.porfolioWeb.Porfolio.Controller.Controller;
-import com.porfolioWeb.Porfolio.model.Educacion;
-import com.porfolioWeb.Porfolio.model.Persona;
-import java.util.ArrayList;
-import java.util.List;*/
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,17 +9,6 @@ public class PorfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PorfolioApplication.class, args);
-                
-                /*Controller contro = new Controller();
-                List<Educacion> listaEducacion = new ArrayList();
-                Persona perso = new Persona();
-                contro.agregarPersona(perso);
-                Educacion edu = new Educacion();
-                contro.agregarEducacion(edu);
-                listaEducacion.add(edu);
-                perso.setListaEducacion(listaEducacion);
-                contro.editEducacion(edu);*/
-                
 	}
 
 }
